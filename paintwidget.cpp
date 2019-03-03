fe3330a4c23ed69bb61754c5fd6c672167b24179
cfcv <cfcv@cin.ufpe.br>
2019-03-03 18:13:48 +0100

    draw circle

--- paintwidget.cpp
+++ paintwidget.cpp
@@ -1,4 +1,5 @@
 #include "paintwidget.h"
+#include <QPainter>
 
 PaintWidget::PaintWidget(QWidget* parent) : QWidget(parent)
 {
@@ -6,5 +7,10 @@ PaintWidget::PaintWidget(QWidget* parent) : QWidget(parent)
 }
 
 void PaintWidget::paintEvent(QPaintEvent* event){
-
+    QPainter painter(this);
+    QPen pen(Qt::blue, 3);
+    painter.setPen(pen);
+    painter.setBrush(Qt::blue);
+    painter.setWindow(0,0,300,300);
+    painter.drawEllipse(QPointF(150, 150), 10, 15);
 }
