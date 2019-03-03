d0cbdfcdf4f3bfb8b8f9022761fa1ed8496fa98e
cfcv <cfcv@cin.ufpe.br>
2019-03-03 16:39:52 +0100

    Camera real time

--- tetrismainwindow.cpp
+++ tetrismainwindow.cpp
@@ -1,17 +1,49 @@
 #include "tetrismainwindow.h"
 #include "ui_tetrismainwindow.h"
-#include "opencv2/imgproc/imgproc.hpp"
-#include "opencv2/highgui/highgui.hpp"
-#include <opencv2/objdetect/objdetect.hpp>
+
+#include <QTimer>
+#include <iostream>
+
+using namespace cv;
 
 TetrisMainWindow::TetrisMainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::TetrisMainWindow)
 {
     ui->setupUi(this);
+    cap = VideoCapture(0);
+    QTimer* timer = new QTimer(this);
+    connect(timer, SIGNAL(timeout()), this, SLOT(cameraThread()));
+    timer->start(10); //milisecondes
 }
 
 TetrisMainWindow::~TetrisMainWindow()
 {
     delete ui;
+}
+
+void TetrisMainWindow::cameraThread(){
+    //VideoCapture cap(0);
+    //#int width=cap.get(CV_CAP_PROP_FRAME_WIDTH);
+    //int height=cap.get(CV_CAP_PROP_FRAME_HEIGHT);
+    //QString cascadePath = "";
+    if (cap.isOpened()) {
+            Mat frame;
+            if (cap.read(frame)) {   // Capture a frame
+                // Flip to get a mirror effect
+                flip(frame,frame,1);
+                // Invert Blue and Red color channels
+                cvtColor(frame,frame,CV_BGR2RGB);
+                // Convert to Qt image
+                QImage img= QImage((const unsigned char*)(frame.data),frame.cols,frame.rows,QImage::Format_RGB888);
+                // Display on label
+                ui->image_label->setPixmap(QPixmap::fromImage(img));
+                // Resize the label to fit the image
+                ui->image_label->resize(ui->image_label->pixmap()->size());
+
+            }
+            else {
+               std::cout << "Error while opening camera." << std::endl;
+            }
+       }
 }
--- tetrismainwindow.h
+++ tetrismainwindow.h
@@ -2,6 +2,9 @@
 #define TETRISMAINWINDOW_H
 
 #include <QMainWindow>
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include <opencv2/objdetect/objdetect.hpp>
 
 namespace Ui {
 class TetrisMainWindow;
@@ -15,8 +18,11 @@ public:
     explicit TetrisMainWindow(QWidget *parent = nullptr);
     ~TetrisMainWindow();
 
+private slots:
+    void cameraThread();
 private:
     Ui::TetrisMainWindow *ui;
+    cv::VideoCapture cap;
 };
 
 #endif // TETRISMAINWINDOW_H
